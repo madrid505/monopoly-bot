10d579ffbd2981ab1e7cda19f534f6758571b6da
madrid505 <anassleet877@gmail.com>
2026-02-22 02:26:26 +0300

    Update interaction.py

--- commands/interaction.py
+++ commands/interaction.py
@@ -3,5 +3,9 @@ from telegram.ext import ContextTypes
 
 async def handle_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
-    if text.lower() == "انطقي":
+    # التاك
+    if text.lower() == "تاك":
+        await update.message.reply_text(f"{update.effective_user.first_name}، انت غير مخول للدخول الى الشبكات الخاصة ❌")
+    # الردود
+    elif text.lower() == "انطقي":
         await update.message.reply_text("هاه؟ 😎")
