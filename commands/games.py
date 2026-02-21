c4818851e699ad9194519e7eefd3b1875d25cd1b
madrid505 <anassleet877@gmail.com>
2026-02-22 02:25:31 +0300

    Update games.py

--- commands/games.py
+++ commands/games.py
@@ -6,3 +6,9 @@ async def roulette(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def photo_game(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("لعبة الصور شغالة 🖼️")
+
+async def challenges(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("لعبة التحديات 🤔")
+
+async def luck_game(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("لعبة الحظ 🍀")
