02b50f47d0e299228dff24456a738ed73aa82154
madrid505 <anassleet877@gmail.com>
2026-02-22 02:26:52 +0300

    Update auto_messages.py

--- commands/auto_messages.py
+++ commands/auto_messages.py
@@ -1,9 +1,22 @@
-import schedule, time
-from telegram.ext import ApplicationBuilder
+import schedule, threading, time
+from config import GROUP_IDS
 
-def schedule_messages(app: ApplicationBuilder):
-    # مثال: نشر اذكار كل ربع ساعة
+def schedule_messages(app):
     def send_adhkar():
-        for group_id in [ -1002695848824, -1003721123319, -1002052564369 ]:
-            app.bot.send_message(chat_id=group_id, text="اذكار 💫")
+        for gid in GROUP_IDS:
+            app.bot.send_message(chat_id=gid, text="اذكار 💫")
+
+    def send_azkar_and_hikam():
+        for gid in GROUP_IDS:
+            app.bot.send_message(chat_id=gid, text="حكم وأدعية 📿")
+
     schedule.every(15).minutes.do(send_adhkar)
+    schedule.every(15).minutes.do(send_azkar_and_hikam)
+
+    def run_scheduler():
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+    thread = threading.Thread(target=run_scheduler)
+    thread.start()
