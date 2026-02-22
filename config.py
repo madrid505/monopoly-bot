82427c32d684557548bfbbbc5d20cc7483637c73
madrid505 <anassleet877@gmail.com>
2026-02-22 05:04:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,4 @@
 # config.py
 
-# توكن البوت
 TOKEN = "8509643139:AAG1-pB6aA883fipwz1qEr_dp5QoSHO4TMs"
-
-# رقم مالك البوت
 OWNER_ID = 5010882230
-
-# رصيد افتراضي
-BANK_DEFAULT = 1000
-ADMIN_BANK = 1000000
-OWNER_BANK = 1000000000
