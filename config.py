2483f6318cdd7a0d86c0ca3bea1a3aeefd37c8b9
madrid505 <anassleet877@gmail.com>
2026-02-22 04:57:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-BOT_TOKEN = "8509643139:AAG1-pB6aA883fipwz1qEr_dp5QoSHO4TMs"
+# config.py
+
+# توكن البوت
+TOKEN = "8509643139:AAG1-pB6aA883fipwz1qEr_dp5QoSHO4TMs"
+
+# رقمك على تلجرام كمالك البوت
 OWNER_ID = 5010882230
 
-GROUP_IDS = [
-    -1002695848824,
-    -1003721123319,
-    -1002052564369
-]
-
-BANK_DEFAULT = 5000
-ADMIN_BANK = 1000000
-OWNER_BANK = 9999999999999
+# إعدادات الرصيد
+BANK_DEFAULT = 1000         # رصيد الاعضاء العاديين
+ADMIN_BANK = 100000         # رصيد المدراء والمشرفين
+OWNER_BANK = 1000000000     # رصيد المالك
